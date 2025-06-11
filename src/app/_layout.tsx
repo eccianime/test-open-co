ab54996e453b7330ebfb36b39c0242bb992a358d
Jean Paul Rojas <33128202+eccianime@users.noreply.github.com>
2025-06-11 16:27:35 -0300

    feat: started project

---
+++ src/app/_layout.tsx
@@ -0,0 +1,22 @@
+import { Stack } from 'expo-router';
+
+import {
+  Poppins_300Light,
+  Poppins_500Medium,
+  Poppins_600SemiBold,
+} from '@expo-google-fonts/poppins';
+import { useFonts } from 'expo-font';
+
+export default function RootLayout() {
+  const [fontsLoaded] = useFonts({
+    Poppins_300Light,
+    Poppins_500Medium,
+    Poppins_600SemiBold,
+  });
+
+  if (!fontsLoaded) {
+    return null;
+  }
+
+  return <Stack />;
+}
